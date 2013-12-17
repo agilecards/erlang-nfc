0d4f9137158549ec5e653489871fd889d7789e45
Peter Whitfield <pete@agilecards.com>
2013-12-17 17:43:21 +1100

    adding basics of C setup

---
+++ c_src/nfc.c
@@ -0,0 +1,39 @@
+//nfc.c
+
+/*
+  methods for interacting with libnfc
+*/
+
+
+// init libnfc
+int init_libnfc() {
+  return 1;
+}
+
+// exit libnfc
+int exit_libnfc() {
+  return 1;
+}
+
+?? list_devices() {
+
+}
+
+// list devices
+
+
+// open a device
+
+
+// close a device
+
+
+// make device idle
+
+
+// setup device as nfc reader (initiator)
+
+
+// start initiator polling for tags
+
+
--- src/erlang_nfc.erl
+++ src/erlang_nfc.erl
@@ -1,15 +1,17 @@
--module(erlang-nfc).
+-module(erlang_nfc).
 
 -export([start/0, start/1, init/1, loop/2]).
+
+-include("erlang_nfc.hrl").
 
 start() ->
   start([]).
 
 start(Options) ->
-  Pid = spawn_link(erlang-nfc, init, [self()]).
+  spawn_link(erlang_nfc, init, [self()]).
 
 init(Pid) ->
-  Port = open_port({spawn, priv_dir()++"/bin/nfc-port"},[binary,{packet,2}]),
+  Port = open_port({spawn, "nfc-port"},[binary,{packet,2}]),
   loop(Pid, Port).
 
 loop(Pid, Port) ->
@@ -17,5 +19,5 @@ loop(Pid, Port) ->
  	{init} ->
  	  Port ! {self(),[?INIT]},
  	  loop(Pid, Port)
-   end
+   end.
 
