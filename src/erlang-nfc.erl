4cd99e2c5f54ef0676a7ead92b4e439fffcc5866
Peter Whitfield <peter.whitfield@viz.com.au>
2013-12-17 11:43:31 +1100

    single message implemented in elrang side

--- src/erlang-nfc.erl
+++ src/erlang-nfc.erl
@@ -1,1 +1,21 @@
 -module(erlang-nfc).
+
+-export([start/0, start/1, init/1, loop/2]).
+
+start() ->
+  start([]).
+
+start(Options) ->
+  Pid = spawn_link(erlang-nfc, init, [self()]).
+
+init(Pid) ->
+  Port = open_port({spawn, priv_dir()++"/bin/nfc-port"},[binary,{packet,2}]),
+  loop(Pid, Port).
+
+loop(Pid, Port) ->
+  receive
+ 	{init} ->
+ 	  Port ! {self(),[?INIT]},
+ 	  loop(Pid, Port)
+   end
+
--- src/erlang-nfc.hrl
+++ src/erlang-nfc.hrl
@@ -2,3 +2,14 @@
 -define(ERLANG_NFC_HRL, true).
 
 % protocol messages
+
+-define(INIT,0).
+-define(EXIT,1).
+-define(LIST_DEVICES,2).
+-define(OPEN_DEVICE,3).
+-define(CLOSE_DEVICE,4).
+-define(IDLE_DEVICE,5).
+-define(INITIATOR_INIT,6).
+-define(INITIATOR_POLL,7).
+
+-endif.
